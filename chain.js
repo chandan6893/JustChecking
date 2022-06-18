187e8c7cb61e5bfdd99a8066bee2bce96463fb6b
Chandan Kumar <krc6893@gmail.com>
2022-06-18 20:17:16 +0530

    First Time

---
+++ chain.js
@@ -0,0 +1,19 @@
+// Using a loop print all odd numbers up to and including n. Don’t include 0.
+for(var i=1;i<=13;i++){
+
+    if(i%2==1)
+    
+    console.log(i);
+}
+
+// ...........Print the sum of all even numbers in a range from a to b. (Including a,b......
+
+       
+// let sum=0
+// for(i=2;i<=8;i++){
+//   if(i%2==0){
+//     sum=sum+i;
+//     }
+// }
+// console.log(sum);
+
